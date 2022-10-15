44b4635005a29a4f0d20792063048d2863920f20
ic-myXMB <106540361+ic-myXMB@users.noreply.github.com>
2022-10-15 05:24:13 -0700

    Update show_captcha.php

--- simple-gd-img-captcha/Upload/show_captcha.php
+++ simple-gd-img-captcha/Upload/show_captcha.php
@@ -3,40 +3,53 @@
 <head>
 <meta charset=UTF-8>
 <title>Simple PHP Generated Captcha</title>
+<style>
+	.captcha-form {
+		padding: 6px;  
+	}
+	.captcha-body {
+		padding: 6px;
+	}	
+	.captcha-image {
+		padding: 6px;
+	}
+	.captcha-request-text {
+		color: teal;
+		font-weight: 400;
+	}
+</style>
 </head>
 <body>
 	<h1>Simple PHP Generated Captcha...</h1>
-	<div>		
-	    <label>Please Enter the Captcha Text:</label>	    
-	    <br />
-	    <br />
-	    <!-- captcha image -->	
-	    <img src="gen_captcha.php" alt="CAPTCHA">	    
-	    <br />
-	    <form name="form" method="POST" action="">
-	    <?php
-	    // start session
-	    session_start();
-	    // safe captcha challenge
-	    $safe_captcha_challenge = htmlspecialchars($_POST['captcha_challenge']);
-	    // define captcha test
-	    $_POST['captcha_current'] = $_SESSION['captcha_current'];
-	    // if captcha challenge and captcha test is true
-	    if (isset($safe_captcha_challenge) && isset($_POST['captcha_current'])) {
-	        // if captcha challenge is equal to captcha test
-	        if ($safe_captcha_challenge === $_POST['captcha_current']) { 	
-	            // echo submitted captcha is correct
-	            echo "<span style=\"color:green;\"><b>Captcha is CORRECT!</b></span><br>";
-	        } else {
-	        	//echo submitted captcha is wrong
-	            echo "<span style=\"color:red;\"><b>Captcha is WRONG! Try again.</b></span><br>";
-	        }
-	    } 
-	    ?> 
-	    <br />
-	        <input type="text" id="captcha_challenge" name="captcha_challenge">
-	        <input type="submit" value="Submit">
-	    </form>
+	<div class="captcha-form">
+	   <div class="captcha-body">		
+	         <span class="captcha-request-text">Please Enter the Captcha Text:</span>	    
+	     <div class="captcha-image">
+	         <!-- captcha image -->	
+	         <img src="gen_captcha.php" alt="CAPTCHA">	    
+	     </div>
+	     <form name="form" method="POST" action="">
+	      <?php
+	         // start session
+	         session_start();
+	         // define captcha current
+	         $_POST['captcha_current'] = $_SESSION['captcha_current'];
+	         // if captcha challenge and captcha current is true
+	           if (isset($_POST['captcha_challenge']) && isset($_POST['captcha_current'])) {
+	               // if captcha challenge is equal to captcha current
+	               if ($_POST['captcha_challenge'] === $_POST['captcha_current']) { 	
+	                 // echo submitted captcha is correct
+	                 echo "<span style=\"color:green;\"><b>Captcha is CORRECT!</b></span><br>";
+	                } else {
+	        	     //echo submitted captcha is wrong
+	                 echo "<span style=\"color:red;\"><b>Captcha is WRONG! Try again.</b></span><br>";
+	                }
+	            } 
+	         ?> 
+	         <input type="text" id="captcha_challenge" name="captcha_challenge">
+	         <input type="submit" value="Submit">
+	      </form>
+	    </div>
 	</div>
 </body>
 </html>
